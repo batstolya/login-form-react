06ee2a64f54243b498e26a4611f62d225ab19464
Anatolii <batstolya@gmail.com>
2022-06-22 14:24:53 +0300

    delete console

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -13,8 +13,6 @@ const emailReducer = (state, action) => {
   if (action.type === "INPUT_BLUR") {
     return { value: state.value, isValid: state.value.includes("@") };
   }
-  console.log("Hello")
-  console.log('Bats Anatolii ')
   return { value: "", isValid: false };
 };
 
